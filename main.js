29fefd28fdc3216288dc0d02a1b71a032ae38f8b
ponvizhi19 <74911744+ponvizhi19@users.noreply.github.com>
2021-07-12 16:35:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+const {BrowserWindow,app} = require("electron");
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        backgroundColor: "rose",
+        webPreferences: {
+            nodeIntegration: false,
+            worldSafeExecuteJavaScript: true,
+            contextIsolation: true
+        }
+    })
+    win.loadFile('index.html');
+}
+
+require('electron-reload')(__dirname,{
+    electron:path.join(__dirname,'node_modules','.bin','electron')
+})
+
+app.whenReady().then(createWindow)
